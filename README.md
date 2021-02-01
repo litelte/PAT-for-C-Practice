bc8220f19c25e4959572419fdc2e93bf5db4ec51
litelte <toget.top@gmail.com>
2021-02-01 17:55:04 +0800

    更换环境为Ubuntu18.04后的第一次更新

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PAT-for-C-Practice
 浙大PAT的C语言练习平台
+目前只是用作练习
---
+++ temp/6.c
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2021-02-01 17:35:14
+ * @LastEditors: litelte
+ * @LastEditTime: 2021-02-01 17:39:08
+ * @FilePath: /PAT-for-C-Practice/temp/6.c
+ * @Description: hello
+ */
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("Hello World!\n");
+    return 0;
+}
