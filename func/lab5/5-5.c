fb0d0fb498bc35e8734fb2a3eecbd8a676fd265b
litelte <toget.top@gmail.com>
2021-02-02 13:00:45 +0800

    更新实验5的目录文件夹

---
+++ func/lab5/5-5.c
@@ -0,0 +1,7 @@
+/*
+ * @Date: 2021-02-02 13:00:00
+ * @LastEditors: litelte
+ * @LastEditTime: 2021-02-02 13:00:09
+ * @FilePath: /PAT-for-C-Practice/func/lab5/5-5.c
+ * @Description: 使用函数求素数的和
+ */
